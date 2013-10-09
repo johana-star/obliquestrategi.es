4ad620e7e91beb0f649b778acdd26624f41be104
Matthew Weitzel <mweitzel@gmail.com>
2013-10-09 14:55:54 -0700

    add layout instead of returning string

--- app/strategies.rb
+++ app/strategies.rb
@@ -2,5 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  File.readlines("strategies.txt").sample
+  @words = File.readlines("strategies.txt").sample
+	@words = make_it_sultry(@words)
+	haml :index
 end
+
+def make_it_sultry(words)
+	words = words.split(" ")
+	words.insert(rand*words.length, "<sub>")
+	words.join(" ")
+end
+
+__END__
+
+@@ layout
+%html
+	%head
+		%link{:rel => :stylesheet, :type=> :"text/css", :href => "/my.css"}
+
+	%body
+		= yield
+
+@@ index
+%div.title
+	=@words
